219dfd1c87e25e7a2997c2f53404458d2bca7450
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2020-01-29 22:12:13 +0100

    Initial proto service

---
+++ src/main/java-mr/11/module-info.java
@@ -0,0 +1,9 @@
+module no.ssb.dapla.data.access.protobuf {
+    requires com.google.common;
+    requires io.grpc;
+    requires grpc.stub;
+    requires com.google.protobuf;
+    requires grpc.protobuf;
+    requires java.annotation;
+    exports no.ssb.dapla.data.access.protobuf;
+}
