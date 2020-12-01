048b75d2eb3c40255a812acf54bc279529fc9ce1
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-12-01 19:00:22 +0100

    Helidon2 (#6)
    * Helidon 2 without grpc (only protobuf messages)
    
    * Open package for reflection
    
    * signed all-metadata fields allowing secure catalog updates

--- src/main/java-mr/11/module-info.java
+++ src/main/java-mr/11/module-info.java
@@ -1,9 +1,5 @@
 module no.ssb.dapla.data.access.protobuf {
-    requires com.google.common;
-    requires io.grpc;
-    requires grpc.stub;
     requires com.google.protobuf;
-    requires grpc.protobuf;
-    requires java.annotation;
     exports no.ssb.dapla.data.access.protobuf;
+    opens no.ssb.dapla.data.access.protobuf;
 }
